a100bc3498bb958025f49ac3dbb716bf85ffbf43
PR154357 <privera@popular-safi.com>
2024-06-26 17:58:55 -0500

    actualizacion de services

--- src/app/modules/shared/services/proveedor.service.ts
+++ src/app/modules/shared/services/proveedor.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const base_url = "https://24p5or4so6.execute-api.ap-southeast-2.amazonaws.com/prod";
+const base_url = "https://nbdnsnw51d.execute-api.ap-southeast-2.amazonaws.com/prod";
 
 @Injectable({
   providedIn: 'root'
